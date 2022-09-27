1bb37c5f1f85b27a2578f1fa0d711276343786e0
josh dietrich <jbdietrich2@gmail.com>
2022-09-27 01:27:26 -0600

    test commit

---
+++ src/config/routes.js
@@ -0,0 +1,15 @@
+"use strict";
+exports.__esModule = true;
+var Home_1 = require("../screens/Home/Home");
+var Teams_1 = require("../screens/Teams/Teams");
+var routes = [
+    {
+        name: 'home',
+        component: Home_1["default"]
+    },
+    {
+        name: 'teams',
+        component: Teams_1["default"]
+    }
+];
+exports["default"] = routes;
