709f8297457cbb4e91315f7a08fdc767ce410594
joshua <jbdietrich2@gmail.com>
2022-11-03 09:20:48 -0600

    trying fling gesture

--- src/screens/playerTurn/playerTurn.tsx
+++ src/screens/playerTurn/playerTurn.tsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect, useContext } from "react";
 import { View, Text, Image } from "react-native";
-import { GestureDetector, Gesture } from "react-native-gesture-handler";
+import {
+  GestureDetector,
+  Gesture,
+  FlingGestureHandler,
+  Directions,
+} from "react-native-gesture-handler";
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
@@ -26,27 +31,42 @@ const PlayerTurnScreen: React.FC<IStackScreenProps> = (props) => {
       transform: [
         { translateX: offset.value.x },
         { translateY: offset.value.y },
-        { scale: withSpring(isPressed.value ? 1.2 : 1) },
+        { scale: withSpring(isPressed.value ? 0.75 : 1) },
       ],
       backgroundColor: isPressed.value ? "yellow" : "blue",
     };
   });
 
+  const flingGesture = Gesture.Fling()
+    .direction(Directions.LEFT | Directions.DOWN)
+    .onBegin((e) => {
+      console.log(e);
+    });
+
   const gesture = Gesture.Pan()
     .onBegin(() => {
       isPressed.value = true;
-      console.log("click");
     })
     .onUpdate((e) => {
-      offset.value = {
-        x: e.translationX + start.value.x,
-        y: e.translationY + start.value.y,
-      };
+      if (e.velocityX - start.value.x > e.velocityY - start.value.y) {
+        console.log("velocityX");
+        offset.value = {
+          x: e.translationX - start.value.x,
+          y: 0,
+        };
+      }
+      if (e.velocityX - start.value.x < e.velocityY - start.value.y) {
+        console.log("velocityY");
+        offset.value = {
+          x: 0,
+          y: e.translationY - start.value.y,
+        };
+      }
     })
     .onEnd(() => {
       start.value = {
-        x: offset.value.x,
-        y: offset.value.y,
+        x: 0,
+        y: 0,
       };
     })
     .onFinalize(() => {
@@ -68,15 +88,9 @@ const PlayerTurnScreen: React.FC<IStackScreenProps> = (props) => {
     setActiveCard(state.deck[0]);
   }, []);
 
-<<<<<<< HEAD
-  useEffect(() => {
-    tickTimer();
-  }, [timer]);
-=======
   // useEffect(() => {
   //   tickTimer();
   // }, [timer]);
->>>>>>> 41b716b35b03b8296a95490b0b7ca92fe831ac33
 
   return (
     <View style={playerTurnStyles.playerTurnPageContainer}>
@@ -87,27 +101,7 @@ const PlayerTurnScreen: React.FC<IStackScreenProps> = (props) => {
         <View style={playerTurnStyles.skipContainer}>
           <Text style={playerTurnStyles.areaText}>Skip</Text>
         </View>
-<<<<<<< HEAD
-        <View style={playerTurnStyles.cardContainer}>
-          <View style={playerTurnStyles.cardImage}>
-            <Image source={{ uri: activeCard?.image_url }} />
-          </View>
-          <View style={playerTurnStyles.cardTitle}>
-            <Text style={playerTurnStyles.cardTitleText}>
-              {activeCard?.card_name}
-            </Text>
-          </View>
-          <View style={playerTurnStyles.cardDescription}>
-            <Text style={playerTurnStyles.cardDescriptionText}>
-              {activeCard?.card_hint}
-            </Text>
-          </View>
-          <View>
-            <Text>{activeCard?.point_value}</Text>
-          </View>
-        </View>
-=======
-        <GestureDetector gesture={gesture}>
+        <GestureDetector gesture={flingGesture}>
           <Animated.View
             style={[playerTurnStyles.cardContainer, animatedStyles]}
           >
@@ -137,7 +131,6 @@ const PlayerTurnScreen: React.FC<IStackScreenProps> = (props) => {
             </View>
           </Animated.View>
         </GestureDetector>
->>>>>>> 41b716b35b03b8296a95490b0b7ca92fe831ac33
         <View style={playerTurnStyles.deckContainer}>
           <Text style={playerTurnStyles.areaText}>Deck</Text>
         </View>
